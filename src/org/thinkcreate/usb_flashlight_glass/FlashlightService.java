6cea06d36abd07cfe23330fee27e0dc3f333508b
DeqingSun <iamseer@gmail.com>
2014-10-23 12:12:11 -0400

    Use a remoteview to replace dialog

--- src/org/thinkcreate/usb_flashlight_glass/FlashlightService.java
+++ src/org/thinkcreate/usb_flashlight_glass/FlashlightService.java
@@ -37,7 +37,7 @@ public class FlashlightService extends Service {
     private static final String LIVE_CARD_TAG = "flashlight";
     private static final String ACTION_USB_PERMISSION = "org.thinkcreate.usb_flashlight_glass.USB_PERMISSION";
     private LiveCard mLiveCard;
-    private RemoteViews mLiveCardView, mLiveCardLoadingView;
+    private RemoteViews mLiveCardView, mLiveCardLoadingView, mLiveCardAlertView;
     private UsbManager mUsbManager;
     private UsbDevice mFlashlight;
     private boolean mIsFlashlightOn = false;
@@ -83,6 +83,11 @@ public class FlashlightService extends Service {
             mLiveCardView = new RemoteViews(getPackageName(), R.layout.flashlight);
             mLiveCardLoadingView = new RemoteViews(getPackageName(), R.layout.loadinglayout);
             mLiveCard.setViews(mLiveCardLoadingView);
+            CardBuilder alertCardBuilder = new CardBuilder(this, CardBuilder.Layout.ALERT);
+            alertCardBuilder.setIcon(R.drawable.ic_cloud_sad_150);
+            alertCardBuilder.setText(R.string.device_not_found);
+            alertCardBuilder.setFootnote(R.string.device_not_found_foot_note);
+            mLiveCardAlertView = alertCardBuilder.getRemoteViews();
             // Create the required menu activity intent
             Intent menuIntent = new Intent(this, MenuActivity.class);
             menuIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -198,9 +203,7 @@ public class FlashlightService extends Service {
     private void updateStatus(int statusResId, int imageResId) {
         if (mLiveCardView != null && mLiveCard != null && mLiveCard.isPublished()) {
             if (statusResId == R.string.device_not_found) {
-                AlertDialog alertDialog = new AlertDialog(getApplicationContext(), R.drawable.ic_cloud_sad_150, R.string.device_not_found, R.string.device_not_found_foot_note, mOnClickListener);
-                alertDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
-                alertDialog.show();
+                mLiveCard.setViews(mLiveCardAlertView);
             } else {
                 mLiveCardView.setTextViewText(R.id.status, getString(statusResId));
                 mLiveCardView.setImageViewResource(R.id.imageStatus, imageResId);
